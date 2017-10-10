c3ddf1f32ec08d2e71365ca350e79e1b4ecadd97
Denis Rykovanov <denis.rykovanov@mail.ru>
2017-10-10 09:10:14 +0300

    reworked to handle work with joystick

--- arduino-bridge.js
+++ arduino-bridge.js
@@ -1,18 +1,66 @@
 let isOpen = false;
 const five = require("johnny-five");
 let board;
-let led;
+let statusLed;
+let rightMotor;
+let leftMotor;
 
 const DEFAULT_CMD_RESULT = 'OK';
+const PIN_LEFT_DIR = 7;
+const PIN_LEFT_SPEED = 6;
+
+const PIN_RIGHT_DIR = 4;
+const PIN_RIGHT_SPEED = 5;
+
+function calcLeftSpeedRation(x) {
+    return x > 0 ? 1 + x : x + 1;
+}
+
+function calcRightSpeedRatio(x) {
+    return x < 0 ? 1 + -x : 1 - x;
+}
+
+function onDirectionCmd({ offset, magnitude }) {
+    console.log('offset', offset);
+    
+    const leftSpeedRatio = calcLeftSpeedRation(offset.x);
+    const rightSpeedRatio = calcRightSpeedRatio(offset.x);
+    const leftSpeed = Math.floor(255 * magnitude * leftSpeedRatio);
+    const rightSpeed = Math.floor(255 * magnitude * rightSpeedRatio);
+
+    console.log(leftSpeed, rightSpeed);
+
+    if (offset.y > 0) {
+        leftMotor.forward(leftSpeed);
+        rightMotor.forward(rightSpeed);
+    } else {
+
+        leftMotor.reverse(leftSpeed);
+        rightMotor.reverse(rightSpeed);
+    }
+}
 
 function configureArduinoChannel() {
 
     board = new five.Board({ repl: false });
     board.on("ready", function() {
         isOpen = true;
-        led = new five.Led(13);
-    });
+        statusLed = new five.Led(13);
 
+        leftMotor = new five.Motor({
+            pins: {
+              pwm: PIN_LEFT_SPEED,
+              dir: PIN_LEFT_DIR
+            }
+        });
+          
+        rightMotor = new five.Motor({
+            pins: {
+              pwm: PIN_RIGHT_SPEED,
+              dir: PIN_RIGHT_DIR
+            }
+        });          
+    });
 
 
     function sendCmdToArduino({ cmd, params }) {
@@ -30,10 +78,10 @@ function configureArduinoChannel() {
             }
 
             try {
-                led.off();
+                statusLed.off();
                 switch (cmd) {
                     case 'direction':
-
+                            onDirectionCmd(params);
                         break;
 
                     default:
@@ -43,7 +91,7 @@ function configureArduinoChannel() {
                 resolveHandler(DEFAULT_CMD_RESULT);
 
             } catch (error) {
-                led.on();
+                statusLed.on();
                 rejectHandler(error);
             }
 
@@ -53,4 +101,4 @@ function configureArduinoChannel() {
     return sendCmdToArduino;
 }
 
-module.exports = { configureArduinoChannel };
+module.exports = { configureArduinoChannel, calcLeftSpeedRation, calcRightSpeedRatio };
