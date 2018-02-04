957c9205c65870bfde63902c7038d9630b5971bc
Denis Rykovanov <drykovanov@wiley.com>
2018-02-04 14:44:14 +0300

    correction for more smooth servos rotation

--- arduino/control-modules/camera.js
+++ arduino/control-modules/camera.js
@@ -1,5 +1,5 @@
 const { PIN_CAMERA_SERVO_1, PIN_CAMERA_SERVO_2 } = require('../cmd-pins');
-const speed = 1000;
+const speed = 500;
 
 function setup({ five }, registerCmd) {
     const cameraServos = new five.Servos([PIN_CAMERA_SERVO_1, PIN_CAMERA_SERVO_2]);
