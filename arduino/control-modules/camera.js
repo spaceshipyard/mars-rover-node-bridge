c287f4acd27118a295a0a84f713462d50a6dd27f
Denis Rykovanov <drykovanov@wiley.com>
2018-02-04 14:41:11 +0300

    correction for more smooth servos rotation

--- arduino/control-modules/camera.js
+++ arduino/control-modules/camera.js
@@ -1,12 +1,12 @@
 const { PIN_CAMERA_SERVO_1, PIN_CAMERA_SERVO_2 } = require('../cmd-pins');
-
+const speed = 1000;
 
 function setup({ five }, registerCmd) {
     const cameraServos = new five.Servos([PIN_CAMERA_SERVO_1, PIN_CAMERA_SERVO_2]);
 
     function onCamera({ offset }) {
-        cameraServos[0].to(offset.x);
-        cameraServos[1].to(offset.y);
+        cameraServos[0].to(offset.x, speed);
+        cameraServos[1].to(offset.y, speed);
     }
 
     const keys = require('../cmd-keys');
