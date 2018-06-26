d11d360c1d5013714f2caa0f0603553b6ec033e1
Denis <denis.rykovanov@mail.ru>
2018-06-26 09:25:22 +0300

    #21 default dispatcher url has been fixed

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const dispatcherUrl = process.env.dispatcherUrl || 'https://localhost:8080'
+const dispatcherUrl = process.env.dispatcherUrl || 'https://localhost'
 const serialPort = process.env.serialPort || undefined
 const targetRoom = process.env.room || 'lobby'
 const statusHandlers = process.env.statusHandlers || 'console-status-handlers'
