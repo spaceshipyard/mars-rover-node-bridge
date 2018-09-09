9dbd39e6ca2234d6ef3b4c99ec67611a7a24de4b
Denis <denis.rykovanov@mail.ru>
2018-09-09 18:27:50 +0300

    [fix] merge servos state with previous call

--- arduino/control-modules/camera.js
+++ arduino/control-modules/camera.js
@@ -1,17 +1,20 @@
 const { PIN_CAMERA_SERVO_1, PIN_CAMERA_SERVO_2 } = require('../cmd-pins')
 const speed = 500
-
+let previousOffset = {x: 0, y: 0}
 function setup ({ five }, registerCmd) {
   const cameraServos = new five.Servos([PIN_CAMERA_SERVO_1, PIN_CAMERA_SERVO_2])
 
   function onCamera ({ offset }) {
+    const newOffset = { ...previousOffset, ...offset }
     cameraServos[0].stop()
     cameraServos[1].stop()
-    cameraServos[0].to(offset.x, speed)
-    cameraServos[1].to(offset.y, speed)
+    cameraServos[0].to(newOffset.x, speed)
+    cameraServos[1].to(newOffset.y, speed)
+    previousOffset = newOffset
   }
 
   const keys = require('../cmd-keys')
+  console.log('camera inited', keys.CMD_KEY_CAMERA)
   registerCmd(keys.CMD_KEY_CAMERA, onCamera)
 }
 
