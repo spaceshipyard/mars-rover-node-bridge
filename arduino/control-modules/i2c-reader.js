f9bca5410ca858df2eab54be7c686aaf321a277e
vnbt <vnbatov@wiley.com>
2019-05-07 14:22:30 +0300

    26 read every 1 second

--- arduino/control-modules/i2c-reader.js
+++ arduino/control-modules/i2c-reader.js
@@ -2,28 +2,32 @@ const eventBus = require('../../events/event-bus')
 const { I2C_DATA } = require('../sensor-keys')
 const { I2C_DATA_RECIEVED } = require('../../events/event-keys')
 
+const READ_DELAY = 1000
+
 function handleChannel (board, channelNumber) {
-  board.io.i2cRead(channelNumber, 27, function (arrayOfBytes) {
-    let message = ''
-    let rawMessage = arrayOfBytes.filter(el => el !== 255)
+  return setInterval(function () {
+    board.io.i2cReadOnce(channelNumber, 27, function (arrayOfBytes) {
+      let message = ''
+      let rawMessage = arrayOfBytes.filter(el => el !== 255)
 
-    if (rawMessage.length) {
-      rawMessage.forEach(code => {
-        const char = String.fromCharCode(code)
-        if (code !== 255) {
-          message = message.concat(char)
-        }
-      })
-    }
+      if (rawMessage.length) {
+        rawMessage.forEach(code => {
+          const char = String.fromCharCode(code)
+          if (code !== 255) {
+            message = message.concat(char)
+          }
+        })
+      }
 
-    if (message.length > 0) {
-      console.log(`i2c message recieved channel=${channelNumber} raw=${rawMessage.join(',')} string="${message}"`)
-      eventBus.emit(I2C_DATA_RECIEVED, { type: I2C_DATA, message, rawMessage })
-    }
-  })
+      if (message.length > 0) {
+        console.log(`i2c message recieved channel=${channelNumber} raw=${rawMessage.join(',')} string="${message}"`)
+        eventBus.emit(I2C_DATA_RECIEVED, { type: I2C_DATA, message, rawMessage })
+      }
+    })
+  }, READ_DELAY)
 }
 function setup ({ board }, registerCmd) {
-  board.io.i2cConfig(1000)
+  board.io.i2cConfig()
   handleChannel(board, 0x01)
   console.log('i2c reader set up -', I2C_DATA_RECIEVED)
 }
